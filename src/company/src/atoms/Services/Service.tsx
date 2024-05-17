a98107fdf7c3d85bbd7fac6d86299cae5e02f12b
lovesitecore <sebastian.winter@sitecore.com>
2024-05-17 18:11:39 +0200

    Implementation of Service Teaser Component including Styling

---
+++ src/company/src/atoms/Services/Service.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentParams, Field, ImageField, Text as JSSText } from '@sitecore-jss/sitecore-jss-nextjs';
+import { servicesTeaserBgSvgJson } from 'src/data/services/serviceTeaserBgSvg';
+
+export interface ServiceFields{
+  Heading: Field<string>;
+  Description: Field<string>;
+  Image: ImageField;
+}
+
+export type ServiceProps =  {
+  params: ComponentParams;
+  fields: ServiceFields;
+  id: number;
+}
+
+export const Default = (props: ServiceProps): JSX.Element => {
+  const objectData = JSON.parse(servicesTeaserBgSvgJson);
+
+  return (
+    <div className="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+      <div className="icon-box iconbox-blue">
+        <div className="icon">
+          <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+            <path stroke="none" stroke-width="0" fill="#f5f5f5" d={objectData[props.id].svg}></path>
+          </svg>
+          <i className="bx bxl-dribbble"></i>
+        </div>
+        <h4><a href=""><JSSText field={props.fields.Heading}/></a></h4>
+        <p><JSSText field={props.fields.Description}/></p>
+      </div>
+    </div>  
+  );
+};
